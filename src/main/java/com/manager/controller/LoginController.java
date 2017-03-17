77b1ecfc0fb468a1aa37302abe7c381187c79c4c
ellenkaijia <136341950@qq.com>
2017-03-17 11:23:39 +0800

    配置logger

--- src/main/java/com/manager/controller/LoginController.java
+++ src/main/java/com/manager/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.manager.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginController {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
 }
