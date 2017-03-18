027ead53536cd7d97960d7fdb5ca0d901c0b7084
ellenkaijia <136341950@qq.com>
2017-03-18 21:23:28 +0800

    配置dubbo

--- src/main/java/com/manager/controller/LoginController.java
+++ src/main/java/com/manager/controller/LoginController.java
@@ -2,10 +2,25 @@ package com.manager.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mhdq.manager.api.service.LoginMsService;
 
 @Controller
+@RequestMapping("/loginControl")
 public class LoginController {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private LoginMsService loginMsService;
+	
+	@RequestMapping("/login")
+    public String login() {
+		logger.info("dsahjdhjaskdhjksahk");
+		loginMsService.sayHello();
+		return "login";
+    }
 }
